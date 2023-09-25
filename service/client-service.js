8477daaf92b0863fe207bcf626af52f2d2f51f2d
Jessyagr97 <jessica.aguirrero@gmail.com>
2023-09-24 18:58:29 -0600

    first commit

---
+++ service/client-service.js
@@ -0,0 +1,25 @@
+//Abrir http (método, url)
+// CRUD - Métodos HTTP
+// Create - POST
+// Read - GET
+// Update - PUT/PATCH
+// Delete - DELETE
+
+//Fetch API
+const listaClientes = () => 
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST", 
+    headers: {
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify({nombre,email, id: uuid.v4()}),
+  }); 
+}
+
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+};
